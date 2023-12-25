4ee781450d49e2580e556f32f3a6712eb833f966
anoopgupta112 <anoopgupta671@gmail.com>
2023-12-25 18:17:37 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Stats Chatbot',
+    version= '0.0.0',
+    author= 'Anoop Chandra',
+    author_email= 'anoopgupta671@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
